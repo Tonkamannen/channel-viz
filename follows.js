d67138c7de1810485d567a3c04aaf7a64a0ccbcf
Tonkamannen <Tonkamannen@users.noreply.github.com>
2013-10-11 01:04:12 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -15,8 +15,8 @@
 	var defaultKey		= 'xGiukdVvujYxceD2psI8V2zJEq5I0tUmULI78qf4JayynCuY', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [1892580236], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'Kolonin', // Replaces Xively logo in the header
-		dataDuration	= '90days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
-		dataInterval	= 10800, // Default interval for data to be displayed (in seconds)
+		dataDuration	= '1days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+		dataInterval	= 120, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
 
